aaf3682eda651b8f868304e3f3107feb2281979d
Dinis Da Silva E Ferreira Da Conceicao <dinda-si@c1r7s3.42lisboa.com>
2023-10-17 10:08:36 +0100

    lib

---
+++ libtf/ft_memmove.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dinda-si <dinda-si@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 11:21:11 by dinda-si          #+#    #+#             */
+/*   Updated: 2023/10/16 16:49:28 by dinda-si         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	char	*d;
+	char	*s;
+	size_t	i;
+
+	i = 0;
+	d = (char *)dest;
+	s = (char *)src;
+	if (s > d)
+	{
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	if (d > s)
+	{
+		while (n-- > 0)
+			d[n] = s[n];
+	}
+	return (dest);
+}
